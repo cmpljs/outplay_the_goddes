0cc4897ee9b4722660734bfc3abded945ce8b3f3
cmpljs <cmplcmplcmpl@gmail.com>
2020-10-06 19:10:35 +0300

    Updates

---
+++ precache-manifest.c8343651e9fad198cb9064a4f3d3fbd0.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee334cf72f608c1e2caf30096cc47dd",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "93d0e2264e6505153531",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.ffd15017.chunk.css"
+  },
+  {
+    "revision": "7d15bf7e89db2d67a5db",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.d268283e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.d268283e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d0e2264e6505153531",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.3bc14266.chunk.js"
+  },
+  {
+    "revision": "5105b1acddf7a89d2a49",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.cb462da8.js"
+  },
+  {
+    "revision": "136dab439961d61f4b9c1f939e07828d",
+    "url": "/%7BGithub-repo-name%7D/static/media/2C.136dab43.png"
+  },
+  {
+    "revision": "1451de5d494406e395215276a2489a7c",
+    "url": "/%7BGithub-repo-name%7D/static/media/2D.1451de5d.png"
+  },
+  {
+    "revision": "ebe434953cb5c82ec5c8e38b8e735489",
+    "url": "/%7BGithub-repo-name%7D/static/media/2H.ebe43495.png"
+  },
+  {
+    "revision": "33c964ae685ff65233d2ac2c35b96072",
+    "url": "/%7BGithub-repo-name%7D/static/media/2S.33c964ae.png"
+  },
+  {
+    "revision": "6ed5bf64f008d72e83c67f81468ab7c5",
+    "url": "/%7BGithub-repo-name%7D/static/media/3C.6ed5bf64.png"
+  },
+  {
+    "revision": "690d5a477dfe9ed93261bd814c0a40d6",
+    "url": "/%7BGithub-repo-name%7D/static/media/3D.690d5a47.png"
+  },
+  {
+    "revision": "7278d51dda50bd4b8c8cadb7fe349953",
+    "url": "/%7BGithub-repo-name%7D/static/media/3H.7278d51d.png"
+  },
+  {
+    "revision": "9638bf9f29919b027765992a81f6d39b",
+    "url": "/%7BGithub-repo-name%7D/static/media/3S.9638bf9f.png"
+  },
+  {
+    "revision": "23c51e0b449e342f8b7f6c8fd57e2137",
+    "url": "/%7BGithub-repo-name%7D/static/media/4C.23c51e0b.png"
+  },
+  {
+    "revision": "89eaac32133444fe40876cb51f697f13",
+    "url": "/%7BGithub-repo-name%7D/static/media/4D.89eaac32.png"
+  },
+  {
+    "revision": "6695f1d514765572dfe7e5dd0863dfce",
+    "url": "/%7BGithub-repo-name%7D/static/media/4H.6695f1d5.png"
+  },
+  {
+    "revision": "5156259e1f30dece1376dc5695a9a1d4",
+    "url": "/%7BGithub-repo-name%7D/static/media/4S.5156259e.png"
+  },
+  {
+    "revision": "3ac88eddd1ac03bfc901de76424b5aba",
+    "url": "/%7BGithub-repo-name%7D/static/media/5C.3ac88edd.png"
+  },
+  {
+    "revision": "24fd440cbb52affc5242a507c9dec4d1",
+    "url": "/%7BGithub-repo-name%7D/static/media/5D.24fd440c.png"
+  },
+  {
+    "revision": "5457f189cabc0476511c7ed1421b419d",
+    "url": "/%7BGithub-repo-name%7D/static/media/5H.5457f189.png"
+  },
+  {
+    "revision": "5ab3d8627d0c4d17c86c73d90817900b",
+    "url": "/%7BGithub-repo-name%7D/static/media/5S.5ab3d862.png"
+  },
+  {
+    "revision": "5c96f40f637a6b2ed34d5e837d81ef7b",
+    "url": "/%7BGithub-repo-name%7D/static/media/6C.5c96f40f.png"
+  },
+  {
+    "revision": "ff1c62992cf49b6164401d1d9f77a20c",
+    "url": "/%7BGithub-repo-name%7D/static/media/6D.ff1c6299.png"
+  },
+  {
+    "revision": "84564e31bf0a0e8bcd5fdd3d4aa12cc3",
+    "url": "/%7BGithub-repo-name%7D/static/media/6H.84564e31.png"
+  },
+  {
+    "revision": "c99b0f04000841e5f3db53d64b3f7034",
+    "url": "/%7BGithub-repo-name%7D/static/media/6S.c99b0f04.png"
+  },
+  {
+    "revision": "4808cc416c976dbd5c3cb629a0f102a5",
+    "url": "/%7BGithub-repo-name%7D/static/media/7C.4808cc41.png"
+  },
+  {
+    "revision": "58d872f063752027e59e15af4d090123",
+    "url": "/%7BGithub-repo-name%7D/static/media/7D.58d872f0.png"
+  },
+  {
+    "revision": "c8475b530ea9f0970ff829c97f3e7abf",
+    "url": "/%7BGithub-repo-name%7D/static/media/7H.c8475b53.png"
+  },
+  {
+    "revision": "82d62864ff67ff7b5e20150316d26872",
+    "url": "/%7BGithub-repo-name%7D/static/media/7S.82d62864.png"
+  },
+  {
+    "revision": "a46a54d93ff6e543497d205db97fd460",
+    "url": "/%7BGithub-repo-name%7D/static/media/8C.a46a54d9.png"
+  },
+  {
+    "revision": "fcafeaf2d610109aa1916208080ab711",
+    "url": "/%7BGithub-repo-name%7D/static/media/8D.fcafeaf2.png"
+  },
+  {
+    "revision": "1e9e59b0d6f684908f3ad7d2fe942a42",
+    "url": "/%7BGithub-repo-name%7D/static/media/8H.1e9e59b0.png"
+  },
+  {
+    "revision": "497eed1efa7e554de5b62bc9eb2b5ae6",
+    "url": "/%7BGithub-repo-name%7D/static/media/8S.497eed1e.png"
+  },
+  {
+    "revision": "4f8d0d8f2ebaeacf99e7134221d6da49",
+    "url": "/%7BGithub-repo-name%7D/static/media/9C.4f8d0d8f.png"
+  },
+  {
+    "revision": "1d495bb1ebf632dca66a538b61bae19a",
+    "url": "/%7BGithub-repo-name%7D/static/media/9D.1d495bb1.png"
+  },
+  {
+    "revision": "33ab7891b4259ea8d8823a352ef38cdf",
+    "url": "/%7BGithub-repo-name%7D/static/media/9H.33ab7891.png"
+  },
+  {
+    "revision": "50caac183ef66afe6cdda1d2e3daafda",
+    "url": "/%7BGithub-repo-name%7D/static/media/9S.50caac18.png"
+  },
+  {
+    "revision": "17ca9f77b395a41c6d45cbff350573bb",
+    "url": "/%7BGithub-repo-name%7D/static/media/AC.17ca9f77.png"
+  },
+  {
+    "revision": "f37d608fb18056bc4f83737d633c5cf5",
+    "url": "/%7BGithub-repo-name%7D/static/media/AD.f37d608f.png"
+  },
+  {
+    "revision": "a762540d55018bcf43ffba8b6343c9c9",
+    "url": "/%7BGithub-repo-name%7D/static/media/AH.a762540d.png"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/%7BGithub-repo-name%7D/static/media/AS.e6a6d2f8.png"
+  },
+  {
+    "revision": "1595a3ea21f452c290cf26af7ae53ede",
+    "url": "/%7BGithub-repo-name%7D/static/media/JC.1595a3ea.png"
+  },
+  {
+    "revision": "d5682eb454510b9cdf54beb9c5a0e5f5",
+    "url": "/%7BGithub-repo-name%7D/static/media/JD.d5682eb4.png"
+  },
+  {
+    "revision": "fdf4ffc434ac567c6b77905cb1b11c8a",
+    "url": "/%7BGithub-repo-name%7D/static/media/JH.fdf4ffc4.png"
+  },
+  {
+    "revision": "89d2c97e62fcaec36c80dbdd725f216f",
+    "url": "/%7BGithub-repo-name%7D/static/media/JS.89d2c97e.png"
+  },
+  {
+    "revision": "aa73cfd0e548d87f4af4a18a8d2c3c8f",
+    "url": "/%7BGithub-repo-name%7D/static/media/KC.aa73cfd0.png"
+  },
+  {
+    "revision": "6b5b30c00309e3cfae486f543bc1809a",
+    "url": "/%7BGithub-repo-name%7D/static/media/KD.6b5b30c0.png"
+  },
+  {
+    "revision": "3b48ae8207b07a1f9477d7e6135ca211",
+    "url": "/%7BGithub-repo-name%7D/static/media/KH.3b48ae82.png"
+  },
+  {
+    "revision": "e8d1a4cfe9d8dca64733c050e65e9dfd",
+    "url": "/%7BGithub-repo-name%7D/static/media/KS.e8d1a4cf.png"
+  },
+  {
+    "revision": "d6296277472d7e9594d0ebee0cb82cdc",
+    "url": "/%7BGithub-repo-name%7D/static/media/QC.d6296277.png"
+  },
+  {
+    "revision": "6940967da8669e3410fefd3a8a33aad9",
+    "url": "/%7BGithub-repo-name%7D/static/media/QD.6940967d.png"
+  },
+  {
+    "revision": "63afa62b47efa84b8b87d3e39ef42bc1",
+    "url": "/%7BGithub-repo-name%7D/static/media/QH.63afa62b.png"
+  },
+  {
+    "revision": "9026849bcbad6fd92f7c3cb8f70e3cd4",
+    "url": "/%7BGithub-repo-name%7D/static/media/QS.9026849b.png"
+  },
+  {
+    "revision": "1963502749c024e1590789bab3632bc9",
+    "url": "/%7BGithub-repo-name%7D/static/media/TC.19635027.png"
+  },
+  {
+    "revision": "39ef422e15761450c33f8cb3ee369ebe",
+    "url": "/%7BGithub-repo-name%7D/static/media/TD.39ef422e.png"
+  },
+  {
+    "revision": "60ab387c8c1c50dd3a75255500218b94",
+    "url": "/%7BGithub-repo-name%7D/static/media/TH.60ab387c.png"
+  },
+  {
+    "revision": "b208cd7f21ce87cf950936ab6fb3e717",
+    "url": "/%7BGithub-repo-name%7D/static/media/TS.b208cd7f.png"
+  },
+  {
+    "revision": "104b50ea5e214c3b338d417a585fd926",
+    "url": "/%7BGithub-repo-name%7D/static/media/aces.104b50ea.png"
+  },
+  {
+    "revision": "e9ac925700fd9ef6c5b0f57d38de57c9",
+    "url": "/%7BGithub-repo-name%7D/static/media/back_cards-07.e9ac9257.png"
+  },
+  {
+    "revision": "8a890151c2b9cbd56a7242210babace6",
+    "url": "/%7BGithub-repo-name%7D/static/media/blue_back.8a890151.png"
+  },
+  {
+    "revision": "db53954a64b0a647a569a1d3964135ce",
+    "url": "/%7BGithub-repo-name%7D/static/media/gray_back.db53954a.png"
+  },
+  {
+    "revision": "80d296e9d8cb90b9802fad8d63b71dc5",
+    "url": "/%7BGithub-repo-name%7D/static/media/green_back.80d296e9.png"
+  },
+  {
+    "revision": "27699599d96905bb74c194591bac114c",
+    "url": "/%7BGithub-repo-name%7D/static/media/honor_clubs.27699599.png"
+  },
+  {
+    "revision": "f815b8c8ee3c11de59b958c8b61af1e9",
+    "url": "/%7BGithub-repo-name%7D/static/media/honor_diamond.f815b8c8.png"
+  },
+  {
+    "revision": "9013ca81c3c0ce7ccc5f097d7d50aca5",
+    "url": "/%7BGithub-repo-name%7D/static/media/honor_heart-14.9013ca81.png"
+  },
+  {
+    "revision": "f5d8c5ce1e0b1612a229633ecf5f67cf",
+    "url": "/%7BGithub-repo-name%7D/static/media/honors_spade-14.f5d8c5ce.png"
+  },
+  {
+    "revision": "44961e95f84a3c3dc34b701ec6a72e6a",
+    "url": "/%7BGithub-repo-name%7D/static/media/purple_back.44961e95.png"
+  },
+  {
+    "revision": "b27beff28165bc95efa3894baf92b637",
+    "url": "/%7BGithub-repo-name%7D/static/media/red_back.b27beff2.png"
+  },
+  {
+    "revision": "28340480d387e857bfd012d05652fd70",
+    "url": "/%7BGithub-repo-name%7D/static/media/yellow_back.28340480.png"
+  }
+]);
